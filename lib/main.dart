cf15ad296a99d72c3d8e3f024e2d0db0f320ddde
junsuPark <krpjs0508@gmail.com>
2024-08-20 11:20:19 +0900

    feedback

--- lib/main.dart
+++ lib/main.dart
@@ -43,26 +43,38 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: <Widget>[
+            /// 무거운 컴포넌트를 클릭하는 느낌
+            /// - 가장 약한 피드백
             OutlinedButton(
               onPressed: () => HapticFeedback.heavyImpact(),
               child: const Text("Heavy Impact"),
             ),
             const SizedBox(height: 20),
+
+            /// 일반적인 컴포넌트를 클릭하는 느낌
+            /// - 중간 피드백
             OutlinedButton(
               onPressed: () => HapticFeedback.mediumImpact(),
               child: const Text("Medium Impact"),
             ),
             const SizedBox(height: 20),
+
+            /// 가벼운 컴포넌트를 클릭하는 느낌
+            /// - 가장 강한 피드백
             OutlinedButton(
               onPressed: () => HapticFeedback.lightImpact(),
               child: const Text("Light Impact"),
             ),
             const SizedBox(height: 20),
+
+            /// 일반적인 세기의 피드백
             OutlinedButton(
               onPressed: () => HapticFeedback.vibrate(),
               child: const Text("Vibrate"),
             ),
             const SizedBox(height: 20),
+
+            /// 미세한 세기의 피드백
             OutlinedButton(
               onPressed: () => HapticFeedback.selectionClick(),
               child: const Text("Selection Click"),
